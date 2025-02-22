c21c242898e651879dc70c3529001897d160761d
ajeigbepaul <pdave4krist@yahoo.com>
2025-02-22 20:23:27 +0100

    header component

--- landingpage/src/App.jsx
+++ landingpage/src/App.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      {/* <header className="flex justify-between items-center px-24 py-4">
+        <div className="flex space-x-8">
+          <h1 className='text-[#505F98] text-sm font-normal'>Home</h1>
+          <h1 className='text-[#505F98] text-sm font-normal'>About</h1>
+          <h1 className='text-[#505F98] text-sm font-normal'>Contact</h1>
+        </div>
+        <h1 className="text-[#37447E] text-2xl font-bold">Landing</h1>
+        <button className='bg-[#111B47] text-white px-10 py-2 rounded-md font-medium'>Buy now</button>
+      </header> */}
+      
+
+    </div>
   )
 }
 
