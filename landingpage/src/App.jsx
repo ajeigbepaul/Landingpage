d0953be233ed8c8620803bd9721a2a7f1422f6c1
chinweu <chinweudeh83@gmail.com>
2025-02-26 21:00:29 +0100

    test learning page

--- landingpage/src/App.jsx
+++ landingpage/src/App.jsx
@@ -12,7 +12,18 @@ const App = () => {
         <h1 className="text-[#37447E] text-2xl font-bold">Landing</h1>
         <button className='bg-[#111B47] text-white px-10 py-2 rounded-md font-medium'>Buy now</button>
       </header> */}
-      
+      <div>
+      <header className="flex justify-between items-center px-24 py-4"> 
+        <div className="flex space-x-8">
+          <h1 className='text-[#58679F] text-sm font-normal'>Home</h1>
+          <h1 className='text-[#58679F] text-sm font-normal'>About</h1>
+          <h1 className='text-[#58679F] text-sm font-normal'>Contact</h1>
+        </div>
+        <h1 className="text-[#762D2D] text-2xl font-bold">landing</h1>
+        <button className='[#65D8DC] text-purple px-8 py-2 rounded-md font-medium'>Buy now</button>
+      </header>
+      </div>
+    
 
     </div>
   )
