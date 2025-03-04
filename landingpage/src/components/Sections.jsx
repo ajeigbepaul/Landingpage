7f94b35756e7a926c4873d248fb408e89409d24a
Emic21 <ecmichaelo@gmail.com>
2025-03-04 08:28:48 +0100

    worked on section 2,3,4 and 5 with footer

---
+++ landingpage/src/components/Sections.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Sections = () => {
+  return (
+    <div>
+      <div className="w-[80%] flex flex-col items-start  space-y-2">
+          <h2 className="text-[#091133] text-3xl font-medium">Light, Fast & Powerful</h2>
+          <p className="text-[#6F7CB2] text-[16px] font-normal">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus
+          </p>
+          <p className="text-[#6F7CB2] text-[16px] font-normal">
+            mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+            sem. Nulla consequat massa quis enim.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Sections
