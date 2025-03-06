da04479a796fdca88d8b0480ed7e1dfee49d7db4
timtee8187 <ezeamara212@gmail.com>
2025-03-06 23:05:19 +0100

    pricing and footer

--- landingpage/src/App.jsx
+++ landingpage/src/App.jsx
@@ -6,16 +6,7 @@ import Section from "./components/Section";
 const App = () => {
   return (
     <div className="relative">
-      <header className="flex justify-between items-center px-24 py-4">
-        <div className="flex space-x-8">
-          <h1 className='text-[#505F98] text-sm font-normal'>Home</h1>
-          <h1 className='text-[#505F98] text-sm font-normal'>About</h1>
-          <h1 className='text-[#505F98] text-sm font-normal'>Contact</h1>
-        </div>
-        <h1 className="text-[#37447E] text-2xl font-bold">Landing</h1>
-        <button className='bg-[#111B47] text-white px-10 py-2 rounded-md font-medium'>Buy now</button>
-      </header> */}
-    <header className="flex flex-col md:flex-row bg-yellow-400 flex-wrap justify-between items-center max-w-screen-xl mx-auto px-6 md:px-24 py-4 space-y-4 md:space-y-0">
+      <header className="flex flex-col md:flex-row bg-blue-400 flex-wrap justify-between items-center max-w-screen-xl mx-auto px-6 md:px-24 py-4 space-y-4 md:space-y-0">
   
     <div className="flex space-x-4 md:space-x-8">
       <h1 className="text-[#000000] text-sm font-normal">Home</h1>
@@ -122,7 +113,40 @@ const App = () => {
             sem. Nulla consequat massa quis enim.`}
         havebutton={true}
       />
-    </div>
+       {/* SECTION FIVE */}
+       <img src="pricebackground.svg" alt="background image" className='absolute'/>
+          <div className='flex flex-col items-center text-center'>
+            
+          <h2 className='text-3xl font-medium leading-[48px] text-black tracking-[0px] mt-60'>A Price To Suit Everyone</h2>
+          <p className='text-base font-normal leading-[26px] text-[#6F7CB2] max-w-lg mt-4'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus </p>
+          <h1 className='text-4xl font-medium text-[#6F7CB2] tracking-[1px] mt-6'>$40</h1>
+          <p className='text-[16px] font-normal leading-[26px] text-[#37447E] tracking-[0px] font-roboto'>UI Design Kit</p>
+          <p className='mt-10 text-lg font-normal text-[#5D6970] font-roboto trcking-[0px]'>See, One price. Simple.</p>
+          <div> <Button title="Purchase Now" /></div>
+          {/* FOOTER */}
+          <footer className='w-full mt-40 py-6 border-t flex justify-between items-start text-sm px-8'>
+            <div className='flex flex-col space-y-2'>
+              <p className='px-24'>&copy; 2023 Your Company</p>
+              <nav className='flex flex-row space-x-8 text-[#6F7CB2] px-24'>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+              </nav>
+              </div>
+              <p className='text-lg font-medium text-[#37447E]'>Landing</p>
+              <div className='flex flex-col space-y-2'>
+             <div> <Button title="Purchase Now" /></div>
+            <div className='flex flex-row space-x-8 px-24'>
+           <img src="facebook.svg" alt="facebook image" />
+           <img src="Linkln.svg" alt="Linkln image" />
+           <img src="X.svg" alt="X image" />
+           <img src="Youtube.svg" alt="Youtube image" />
+           <img src="instagram.svg" alt="instagram image" />
+            </div>
+            </div>
+          </footer>
+        </div>
+        </div>
   );
 };
 
